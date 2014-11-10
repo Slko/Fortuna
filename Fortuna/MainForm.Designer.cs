06756cad517e0bc67fe7ffd73844cf7472e7c0b0
Stanislav Skobelkin <slko@slko.net>
2014-11-11 01:55:54 +0400

    Initial commit

---
+++ Fortuna/MainForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Fortuna
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusDescriptionLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.updateDataCheckBox = new System.Windows.Forms.CheckBox();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.updatePeriodLabel = new System.Windows.Forms.Label();
+            this.updatePeriodNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.msLabel = new System.Windows.Forms.Label();
+            this.betsGridView = new System.Windows.Forms.DataGridView();
+            this.numbers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bets = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.roundDescriptionLabel = new System.Windows.Forms.Label();
+            this.roundLabel = new System.Windows.Forms.Label();
+            this.gameProgressBar = new System.Windows.Forms.ProgressBar();
+            this.updateProgressBarTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.updatePeriodNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.betsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusDescriptionLabel
+            // 
+            this.statusDescriptionLabel.AutoSize = true;
+            this.statusDescriptionLabel.Location = new System.Drawing.Point(12, 9);
+            this.statusDescriptionLabel.Name = "statusDescriptionLabel";
+            this.statusDescriptionLabel.Size = new System.Drawing.Size(64, 13);
+            this.statusDescriptionLabel.TabIndex = 0;
+            this.statusDescriptionLabel.Text = "Состояние:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(137, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(79, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Не подключен";
+            // 
+            // updateDataCheckBox
+            // 
+            this.updateDataCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.updateDataCheckBox.AutoSize = true;
+            this.updateDataCheckBox.Checked = true;
+            this.updateDataCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.updateDataCheckBox.Location = new System.Drawing.Point(304, 333);
+            this.updateDataCheckBox.Name = "updateDataCheckBox";
+            this.updateDataCheckBox.Size = new System.Drawing.Size(149, 17);
+            this.updateDataCheckBox.TabIndex = 2;
+            this.updateDataCheckBox.Text = "Получать новые данные";
+            this.updateDataCheckBox.UseVisualStyleBackColor = true;
+            this.updateDataCheckBox.CheckedChanged += new System.EventHandler(this.updateDataCheckBox_CheckedChanged);
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Enabled = true;
+            this.updateTimer.Interval = 1000;
+            this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+            // 
+            // updatePeriodLabel
+            // 
+            this.updatePeriodLabel.AutoSize = true;
+            this.updatePeriodLabel.Location = new System.Drawing.Point(12, 31);
+            this.updatePeriodLabel.Name = "updatePeriodLabel";
+            this.updatePeriodLabel.Size = new System.Drawing.Size(122, 13);
+            this.updatePeriodLabel.TabIndex = 3;
+            this.updatePeriodLabel.Text = "Интервал обновления:";
+            // 
+            // updatePeriodNumericUpDown
+            // 
+            this.updatePeriodNumericUpDown.Location = new System.Drawing.Point(140, 29);
+            this.updatePeriodNumericUpDown.Maximum = new decimal(new int[] {
+            60000,
+            0,
+            0,
+            0});
+            this.updatePeriodNumericUpDown.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.updatePeriodNumericUpDown.Name = "updatePeriodNumericUpDown";
+            this.updatePeriodNumericUpDown.Size = new System.Drawing.Size(54, 20);
+            this.updatePeriodNumericUpDown.TabIndex = 4;
+            this.updatePeriodNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.updatePeriodNumericUpDown.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.updatePeriodNumericUpDown.ValueChanged += new System.EventHandler(this.updatePeriodNumericUpDown_ValueChanged);
+            // 
+            // msLabel
+            // 
+            this.msLabel.AutoSize = true;
+            this.msLabel.Location = new System.Drawing.Point(200, 31);
+            this.msLabel.Name = "msLabel";
+            this.msLabel.Size = new System.Drawing.Size(21, 13);
+            this.msLabel.TabIndex = 5;
+            this.msLabel.Text = "мс";
+            // 
+            // betsGridView
+            // 
+            this.betsGridView.AllowUserToAddRows = false;
+            this.betsGridView.AllowUserToDeleteRows = false;
+            this.betsGridView.AllowUserToResizeColumns = false;
+            this.betsGridView.AllowUserToResizeRows = false;
+            this.betsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.betsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.betsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numbers,
+            this.Bets});
+            this.betsGridView.Location = new System.Drawing.Point(12, 69);
+            this.betsGridView.MultiSelect = false;
+            this.betsGridView.Name = "betsGridView";
+            this.betsGridView.ReadOnly = true;
+            this.betsGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.betsGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.betsGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.betsGridView.Size = new System.Drawing.Size(441, 229);
+            this.betsGridView.TabIndex = 6;
+            // 
+            // numbers
+            // 
+            this.numbers.HeaderText = "Сектор";
+            this.numbers.Name = "numbers";
+            this.numbers.ReadOnly = true;
+            // 
+            // Bets
+            // 
+            this.Bets.HeaderText = "Ставка";
+            this.Bets.Name = "Bets";
+            this.Bets.ReadOnly = true;
+            // 
+            // roundDescriptionLabel
+            // 
+            this.roundDescriptionLabel.AutoSize = true;
+            this.roundDescriptionLabel.Location = new System.Drawing.Point(12, 53);
+            this.roundDescriptionLabel.Name = "roundDescriptionLabel";
+            this.roundDescriptionLabel.Size = new System.Drawing.Size(43, 13);
+            this.roundDescriptionLabel.TabIndex = 7;
+            this.roundDescriptionLabel.Text = "Тираж:";
+            // 
+            // roundLabel
+            // 
+            this.roundLabel.AutoSize = true;
+            this.roundLabel.Location = new System.Drawing.Point(61, 53);
+            this.roundLabel.Name = "roundLabel";
+            this.roundLabel.Size = new System.Drawing.Size(27, 13);
+            this.roundLabel.TabIndex = 8;
+            this.roundLabel.Text = "N/A";
+            // 
+            // gameProgressBar
+            // 
+            this.gameProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameProgressBar.Location = new System.Drawing.Point(15, 303);
+            this.gameProgressBar.Name = "gameProgressBar";
+            this.gameProgressBar.Size = new System.Drawing.Size(438, 23);
+            this.gameProgressBar.Step = 1;
+            this.gameProgressBar.TabIndex = 9;
+            // 
+            // updateProgressBarTimer
+            // 
+            this.updateProgressBarTimer.Enabled = true;
+            this.updateProgressBarTimer.Interval = 1000;
+            this.updateProgressBarTimer.Tick += new System.EventHandler(this.updateProgressBarTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 359);
+            this.Controls.Add(this.gameProgressBar);
+            this.Controls.Add(this.roundLabel);
+            this.Controls.Add(this.roundDescriptionLabel);
+            this.Controls.Add(this.betsGridView);
+            this.Controls.Add(this.msLabel);
+            this.Controls.Add(this.updatePeriodNumericUpDown);
+            this.Controls.Add(this.updatePeriodLabel);
+            this.Controls.Add(this.updateDataCheckBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.statusDescriptionLabel);
+            this.Name = "MainForm";
+            this.Text = "Fortuna";
+            ((System.ComponentModel.ISupportInitialize)(this.updatePeriodNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.betsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statusDescriptionLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.CheckBox updateDataCheckBox;
+        private System.Windows.Forms.Timer updateTimer;
+        private System.Windows.Forms.Label updatePeriodLabel;
+        private System.Windows.Forms.NumericUpDown updatePeriodNumericUpDown;
+        private System.Windows.Forms.Label msLabel;
+        private System.Windows.Forms.DataGridView betsGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numbers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bets;
+        private System.Windows.Forms.Label roundDescriptionLabel;
+        private System.Windows.Forms.Label roundLabel;
+        private System.Windows.Forms.ProgressBar gameProgressBar;
+        private System.Windows.Forms.Timer updateProgressBarTimer;
+    }
+}
+
---
+++ Fortuna/MainForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Fortuna
+{
+    public partial class MainForm : Form
+    {
+        private const string dataSourceUrl = "http://bm.inbet.cc/fortuna_json";
+        private int roundTime = 120;
+        private int lastPacketVersion = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void updateDataCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            updateTimer.Enabled = updateDataCheckBox.Checked;
+        }
+
+        private async void updateTimer_Tick(object sender, EventArgs e)
+        {
+            JObject data;
+            try
+            {
+                data = await GetJson(String.Format("{0}?vsn={1}", dataSourceUrl, lastPacketVersion));
+            }
+            catch (WebException ex)
+            {
+                statusLabel.Text = "Ошибка подключения: " + ex.Message;
+                return;
+            }
+            catch (JsonException ex)
+            {
+                statusLabel.Text = "Ошибка обработки данных от сервера: " + ex.Message;
+                return;
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = "Неизвестная ошибка: " + ex.Message;
+                return;
+            }
+            lastPacketVersion = data["state"].Value<int>("vsn");
+            string state = data["state"].Value<string>("state");
+            roundLabel.Text = data["state"].Value<int>("round").ToString();
+            switch (state)
+            {
+                case "betting":
+                    statusLabel.Text = "Приём ставок";
+                    int scrollOffset = betsGridView.FirstDisplayedScrollingRowIndex;
+                    betsGridView.Rows.Clear();
+                    JObject stakesObject = (JObject)data["state"]["stakes"];
+                    IEnumerable<KeyValuePair<int, float>> stakes = stakesObject.Properties()
+                                                                               .Select(x => new KeyValuePair<int, float>(int.Parse(x.Name), x.Value.Value<float>()))
+                                                                               .OrderBy(x => x.Key);
+                    betsGridView.Rows.AddRange(stakes.Select(x => 
+                        {
+                            DataGridViewRow row = new DataGridViewRow();
+                            row.Cells.Add(new DataGridViewTextBoxCell()
+                                {
+                                    Value = x.Key.ToString(),
+                                });
+                            row.Cells.Add(new DataGridViewTextBoxCell()
+                                {
+                                    Value = x.Value.ToString(),
+                                });
+                            return row;
+                        }).ToArray());
+                    if ((scrollOffset != -1) && (betsGridView.RowCount > scrollOffset))
+                    {
+                        betsGridView.FirstDisplayedScrollingRowIndex = scrollOffset;
+                    }
+                    break;
+                case "playing":
+                    statusLabel.Text = "Розыгрыш";
+                    break;
+                default:
+                    statusLabel.Text = state;
+                    break;
+            }
+            roundTime = (int)(data["state"].Value<double>("time2switch") - data["state"].Value<double>("stamp"));
+            int tick = roundTime - (int)Math.Round(data["state"].Value<double>("time2switch") - data.Value<double>("time"));
+            gameProgressBar.Maximum = roundTime;
+            gameProgressBar.Value = tick;
+        }
+
+        async Task<JObject> GetJson(string url)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            string jsonString;
+
+            using (WebResponse response = await request.GetResponseAsync())
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                jsonString = await reader.ReadToEndAsync();
+            }
+
+            return JObject.Parse(jsonString);
+        }
+
+        private void updatePeriodNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            updateTimer.Interval = (int)updatePeriodNumericUpDown.Value;
+        }
+
+        private void updateProgressBarTimer_Tick(object sender, EventArgs e)
+        {
+            gameProgressBar.Value = Math.Min(gameProgressBar.Value + 1, gameProgressBar.Maximum);
+        }
+    }
+}
